c5339a699f03a2cf46555e57dd395290c3861636
Paulo207 <andreairesw@gmail.com>
2023-03-10 12:27:58 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('form');
+const tabela = document.querySelector('table tbody');
+
+form.addEventListener('submit'), (event) => {
+event.preventDefault();
+}
+
+const nome = form.elements.nome.value;
+const telefone = form.elements.telefone.value;
+
+const novaLinha = document.createElement('tr');
+const colunaNome = document.createElement('td');
+const colunaTelefone = document.createElement('td');
+colunaNome.textContent = nome;
+colunaTelefone.textContent = telefone;
+novaLinha.appendChild(colunaNome);
+novaLinha.appendChild(colunaTelefone);
+
+tabela.appendChild(novaLinha);
